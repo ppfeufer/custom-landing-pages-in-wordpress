eb62f388610bd35c9ad6bbb1502e4a1196d20bc0
Brian Sexton <brian.sexton@gmail.com>
2017-08-09 01:30:31 -0700

    Add a second simple landing page template.

---
+++ simple-landing-page-2.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: Simple Landing Page 2
+Template Post Type: post, page
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Simple Landing Page 2</title>
+</head>
+<body>
+	<h1>Simple Landing Page 2</h1>
+	<p>This is a second simple landing page. Woo hoo!</p>
+</body>
+</html>
