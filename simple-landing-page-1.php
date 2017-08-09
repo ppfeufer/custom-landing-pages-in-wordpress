522b0c64dd26246ad5e4ce41ad7daf783922c7ea
Brian Sexton <brian.sexton@gmail.com>
2017-08-09 01:25:36 -0700

    Add some placeholder content to the first simple landing page template.

--- simple-landing-page-1.php
+++ simple-landing-page-1.php
@@ -4,3 +4,15 @@ Template Name: Simple Landing Page 1
 Template Post Type: post, page
 */
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Simple Landing Page 1</title>
+</head>
+<body>
+	<h1>Simple Landing Page 1</h1>
+	<p>This is a simple landing page. Wheee!</p>
+</body>
+</html>
