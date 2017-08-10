8d4be6f0b213fe0b50f8b8711a5e57b865144148
Brian Sexton <brian.sexton@gmail.com>
2017-08-09 23:52:53 -0700

    Add a new landing page template.

---
+++ webenertia-wordpress-custom-theme/landing-page-1.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Template Name: Landing Page 1
+Template Post Type: post, page
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Landing Page 1</title>
+</head>
+<body>
+
+	<div class="content">
+
+		<div class="splash">
+
+			<h1>Landing Page 1</h1>
+
+		</div>
+
+		<div class="customer-options">
+
+			<!-- The customer options will go here. -->
+
+		</div>
+
+		<main>
+
+			<!-- The featured image will go here. -->
+
+			<!-- The content will go here. -->
+
+		</main>
+
+	</div>
+
+</body>
+</html>
